10c18f72656938716bb89f8d3f36ac5d489fc411
Felix Peppert <felix.peppert@gmail.com>
2017-06-18 15:43:14 +0200

    Update pyfaidx: Add hack in build.sh to skip forbidden  setuptools download

--- recipes/pyfaidx/0.4.9.2/build.sh
+++ recipes/pyfaidx/0.4.9.2/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+# does not work without the extra options
+$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
 # Add more build steps here, if they are necessary.
 
